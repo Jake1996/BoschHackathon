abdfec20e9f794cedca5a6e0c9888332d45ecfb8
root <root@Jatin-PC.localdomain>
2016-10-02 03:17:19 +0530

    s2

--- rasp.py
+++ rasp.py
@@ -3,14 +3,14 @@
 import socket
 import time
 import sys
-#import serial
-#ser = serial.Serial(str(sys.argv[0]),9600)
+import serial
+ser = serial.Serial(str(sys.argv[1]),9600)
 s = socket.socket()
-s.bind(('', int(sys.argv[1])))
+s.bind(('', int(sys.argv[2])))
 s.listen(5)
 c, addr = s.accept() #use c from here on c is controller
 while True:
     toRun = c.recv(1024)
-#    ser.send(toRun)
+    ser.write(toRun)
     print toRun
 s.close()
